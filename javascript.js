d43ea9d161a5d92e0f9f747e3c8ce8e33467d50c
Alex Badila <abadila123@gmail.com>
2024-09-13 13:58:41 -0400

    Added playRound and playGame
    Added the function play round, which plays a round
    of rock-paper-scissors between the player and the
    computer through conditionals. Also added the
    playGame function, which plays five rounds of the
    game and decides the winner at the end.

--- javascript.js
+++ javascript.js
@@ -1,4 +1,6 @@
-getComputerChoice = () => {
+
+
+const getComputerChoice = () => {
     // Choose a random number between 1 and 3
     let choiceNum = Math.round(Math.random() * 2) + 1;
     // Translate that number into rock, paper, or scissors and return it
@@ -13,7 +15,7 @@ getComputerChoice = () => {
     }
 }
 
-getHumanChoice = () => {
+const getHumanChoice = () => {
     // Prompt the user for the choice
     let humanChoice = prompt("Choose between rock, paper, or scissors.")
     // If it's a valid choice, return
@@ -26,10 +28,82 @@ getHumanChoice = () => {
     }
 }
 
-// Declare the variables for the score and initialize them to 0
-let humanScore = 0;
-let computerScore = 0;
+const playGame = () => {
+    // Declare the variables for the score and initialize them to 0
+    let humanScore = 0;
+    let computerScore = 0;
 
-let computerChoice = getComputerChoice();
+    // Declare a counter for the loop
+    let counter = 0;
 
-console.log(getHumanChoice());
+    // Declare playRound as a function inside playGame
+    const playRound = (humanChoice, computerChoice) => {
+        // Lowercase the values so case doesn't matter
+        humanChoice = humanChoice.toLowerCase();
+        computerChoice = computerChoice.toLowerCase();
+
+        // Play the game rock-paper-scissors between the human and the computer
+        // Go through each possibility one by one until one matches
+        // Then increment the corresponding score and return the appropriate string
+        if(humanChoice == "rock" && computerChoice == "paper") {
+            computerScore++;
+            return "You lose! Paper beats rock!";
+        }
+        else if(humanChoice == "rock" && computerChoice == "scissors") {
+            humanScore++;
+            return "You win! Rock beats scissors!";
+        }
+        else if(humanChoice == "rock" && computerChoice == "rock") {
+            return "Tie game!";
+        }
+        else if(humanChoice == "paper" && computerChoice == "scissors") {
+            computerScore++;
+            return "You lose! Scissors beats paper!";
+        }
+        else if(humanChoice == "paper" && computerChoice == "rock") {
+            humanScore++;
+            return "You win! Paper beats rock!";
+        }
+        else if(humanChoice == "paper" && computerChoice == "paper") {
+            return "Tie game!";
+        }
+        else if(humanChoice == "scissors" && computerChoice == "rock") {
+            computerScore++;
+            return "You lose! Rock beats scissors!";
+        }
+        else if(humanChoice == "scissors" && computerChoice == "paper") {
+            humanScore++;
+            return "You win! Scissors beats paper!";
+        }
+        else if(humanChoice == "scissors" && computerChoice == "scissors") {
+            return "Tie game!";
+        }
+        else {
+            return "Invalid input";
+        }
+    }
+
+    // Loop through playRound 5 times
+    while(counter < 5) {
+        // Get the choices from the humand and the computer
+        let humanChoice = getHumanChoice();
+        let computerChoice = getComputerChoice();
+
+        console.log(playRound(humanChoice, computerChoice));
+        counter++;
+    }
+
+    if(humanScore > computerScore) {
+        console.log("You win!")
+    }
+    else if(humanScore < computerScore) {
+        console.log("You lose!")
+    }
+    else {
+        console.log("It's a tie!")
+    }
+}
+
+playGame();
+
+
